d8076117a97b7d1fc1a72c82ae48ce66d2173eb1
gryzun33 <yakushevao777@gmail.com>
2024-04-04 00:52:32 +0300

    feat: add tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -229,8 +229,12 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  // throw new Error('Not implemented');
+  function partial(...args2) {
+    return fn(...args1, ...args2);
+  }
+  return partial;
 }
 
 /**
@@ -250,8 +254,15 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  // throw new Error('Not implemented');
+  let count = startFrom;
+  function foo() {
+    const result = count;
+    count += 1;
+    return result;
+  }
+  return foo;
 }
 
 module.exports = {
